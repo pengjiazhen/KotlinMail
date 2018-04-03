3c7f8fe142606eddba8493f1495557c9b1487786
pengjiazhen <1752234827@qq.com>
2018-04-03 21:36:58 +0800

    add rxjava

--- UserCenterModule/src/main/java/com/bj/user/presenter/RegisterPresenter.kt
+++ UserCenterModule/src/main/java/com/bj/user/presenter/RegisterPresenter.kt
@@ -2,15 +2,32 @@ package com.bj.user.presenter
 
 import com.bj.base.presenter.BasePresenter
 import com.bj.user.view.RegisterView
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by jiazhen on 2018/4/2.
  * Desc:
  */
-class RegisterPresenter : BasePresenter<RegisterView>(){
+class RegisterPresenter : BasePresenter<RegisterView>() {
 
-    fun doRegister(phone:String,code:String){
-        mView.onRegisterSuccess()
+    fun doRegister(phone: String, code: String) {
+        Observable.create<Boolean> {
+            it.onNext(true)
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    if (it)
+                        mView.onRegisterSuccess()
+                },{
+
+                },{
+
+                })
+
     }
 
 }
+
+
